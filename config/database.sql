d99b604c9d78844e8aa8d395a658d49c2caff8a6
Diego <diegojojoa398@gmail.com>
2026-03-06 11:59:45 -0500

    database firts backup

---
+++ config/database.sql
@@ -0,0 +1,9 @@
+create table users(
+id bigint
+ firstname      | character varying(15) |              | not null |
+ lastname       | character varying(15) |              | not null |
+ email          | character varying(50) |              | not null |
+ password       | character(20)         |              | not null |
+ mobile_phone   | character varying(20) |              | not null |
+ password_again | character(20)         |              | not null |
+)
