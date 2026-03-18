25d7d224343a3bd39f150b0cd5de3c00694dfd6a
Diego <diegojojoa398@gmail.com>
2026-03-18 08:38:55 -0500

    pus conections

--- config/database.php
+++ config/database.php
@@ -7,25 +7,41 @@ $LOCAL_PASSWORD  = 'unicesmag';
 $LOCAL_PORT      = '5432';
 
 // //supabase Database configuratin 
-$SUPA_HOST      = '';
-$SUPA_DBNAME    = '';
-$SUPA_USERNAME  = '';
-$SUPA_PASSWORD  = '';
-$SUPA_PORT      = '';
+$SUPA_HOST      = 'aws-1-us-east-1.pooler.supabase.com';
+$SUPA_DBNAME    = 'postgres';
+$SUPA_USERNAME  = 'postgres.uxdbelufuuhvgusqmiqq';
+$SUPA_PASSWORD  = 'unicesmag@@';
+$SUPA_PORT      = '6543';
 
-$data_connection ="
+$local_data_connection ="
 host=$LOCAL_HOST  
 dbname=$LOCAL_DBNAME 
 user=$LOCAL_USERNAME
 password=$LOCAL_PASSWORD
 port=$LOCAL_PORT
 ";
-$conn =pg_connect($data_connection);
-if(!$conn){
-echo "Error: Unable to connet to database.";
+
+$supa_data_connection ="
+host=$SUPA_HOST  
+dbname=$SUPA_DBNAME 
+user=$SUPA_USERNAME
+password=$SUPA_PASSWORD
+port=$SUPA_PORT
+";
+//local conection
+$local_conn =pg_connect($local_data_connection);
+if(!$local_conn){
+echo "Error: Unable to connet to local database.";
 exit();
 }else{
-echo "success connection !!!";
-
+echo "local success connection !!!";
+}
+//supa conection
+$supa_conn =pg_connect($supa_data_connection);
+if(!$supa_conn){
+echo "<br>Error: Unable to connet to supa database.";
+exit();
+}else{
+echo "<br>supa success connection !!!";
 }
 ?>
---
+++ config/example.php
@@ -0,0 +1,14 @@
+<?php
+echo "hello word. we're here";
+$num1 = 20;
+$num2 = 10;
+$add = $num1 + $num2;
+echo "<br>";
+echo "<font color ='BLUE'><b>The addition is:</b></font>".$add;
+
+if($num1 > $num2){
+echo "<br> you win";
+}else{
+echo "<br> you lost";
+}
+?>
