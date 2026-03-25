3dfe62f5c10ed39fd6d4b667e640ef2c010036b8
Diego <diegojojoa398@gmail.com>
2026-03-25 08:49:34 -0500

    nueva tabla de database

--- config/database.sql
+++ config/database.sql
@@ -1,9 +1,15 @@
 create table users(
-id bigint
- firstname      | character varying(15) |              | not null |
- lastname       | character varying(15) |              | not null |
- email          | character varying(50) |              | not null |
- password       | character(20)         |              | not null |
- mobile_phone   | character varying(20) |              | not null |
- password_again | character(20)         |              | not null |
-)
+id             SERIAL PRIMARY KEY NOT NULL,
+firstname      VARCHAR(50)   NOT NULL, 
+lasname        VARCHAR(50)   NOT NULL,
+email          VARCHAR(100)  NOT NULL UNIQUE,
+password       TEXT          NOT NULL,
+mobile_phone   VARCHAR(20)   NOT NULL UNIQUE,
+address        VARCHAR(100)  NULL,
+birthday       DATE          NULL,
+status         BOOLEAN       DEFAULT TRUE,
+gender         CHAR(1)       DEFAULT NULL,
+created_at     TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+updated_at     TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+deleted_at     TIMESTAMP     DEFAULT NULL
+);
--- src/signup.php
+++ src/signup.php
@@ -7,11 +7,12 @@ $L_name  =$_POST ['lname' ];
 $E_mail  =$_POST ['email' ];
 $M_phone =$_POST ['mphone'];
 $p_sswd  =$_POST ['passwd'];
+$enc_pass = md5($passwd);
 
 // Query to insert into SQL. 
 
 $sql="INSERT INTO users (firstname,lastname,email,mobile_phone,password)
-values('$f_name', '$L_name','$E_mail', '$M_phone', '$p_sswd')";
+values('$f_name', '$L_name','$E_mail', '$M_phone', '$enc_pass')";
 
 //Execute query
 pg_query($sql);
