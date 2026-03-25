0895eb4c6436509abf1fd8c095396f3bc1bbe1ac
Diego <diegojojoa398@gmail.com>
2026-03-25 08:52:51 -0500

    nueva tabla de database

--- config/database.sql
+++ config/database.sql
@@ -1,7 +1,7 @@
 create table users(
 id             SERIAL PRIMARY KEY NOT NULL,
 firstname      VARCHAR(50)   NOT NULL, 
-lasname        VARCHAR(50)   NOT NULL,
+lastname        VARCHAR(50)  NOT NULL,
 email          VARCHAR(100)  NOT NULL UNIQUE,
 password       TEXT          NOT NULL,
 mobile_phone   VARCHAR(20)   NOT NULL UNIQUE,
